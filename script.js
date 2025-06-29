89f0e7df3e98b759b06eb3f027bd6890cf8fc8ca
Aniket Tikudave <anikettikudave187@gmail.com>
2025-06-29 16:12:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+	let dropdown=document.getElementById('colorSelect');
+	let removeBtn=document.querySelector('input[type="button"]');
+	
+	
+	removeBtn.addEventListener("click",()=>{
+		const colorIdx=dropdown.selectedIndex;
+	
+		if(colorIdx>=0){
+			dropdown.remove(colorIdx);
+		}else{
+			alert('list is empty');
+		}
+	});
